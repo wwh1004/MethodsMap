061c5f6841f47846b5a870fde6da825bd97f55c4
文煌 <wwh1004@hotmail.com>
2018-07-24 18:27:09 +0800

    update

--- MethodsMap/Properties/AssemblyInfo.cs
+++ MethodsMap/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2018 Wwh / NetCracker")]
 [assembly: ComVisible(false)]
 [assembly: Guid("db51a93e-6c87-4eba-b569-8f3d523083e9")]
-[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("1.3.0.*")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
