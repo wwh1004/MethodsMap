3e1ed91fff5f350b6b10669b8dfcfc4933722428
文煌 <wwh1004@hotmail.com>
2018-07-30 18:54:45 +0800

    update copyright

--- MethodsMap/Properties/AssemblyInfo.cs
+++ MethodsMap/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MethodsMap")]
 [assembly: AssemblyProduct("MethodsMap")]
-[assembly: AssemblyCopyright("Copyright © 2018 Wwh / NetCracker")]
+[assembly: AssemblyCopyright("Copyright © 2018 Wwh / NCK")]
 [assembly: ComVisible(false)]
 [assembly: Guid("db51a93e-6c87-4eba-b569-8f3d523083e9")]
 [assembly: AssemblyVersion("1.4.0.1")]
