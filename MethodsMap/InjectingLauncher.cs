9ffa4402e9caf3656e80b306ddd64490050482dd
文煌 <wwh1004@hotmail.com>
2018-08-29 17:19:10 +0800

    update

--- MethodsMap/InjectingLauncher.cs
+++ MethodsMap/InjectingLauncher.cs
@@ -1,8 +1,31 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 
 namespace MethodsMap {
 	public static class InjectingLauncher {
 		public static int Launch(string arg) {
+			Module module;
+
+			module = Assembly.GetExecutingAssembly().ManifestModule;
+			for (int i = 0x06000001; i < int.MaxValue; i++) {
+				MethodBase methodBase;
+
+				try {
+					methodBase = module.ResolveMethod(i);
+				}
+				catch (ArgumentOutOfRangeException) {
+					// 遍历完所有方法
+					break;
+				}
+				catch {
+					continue;
+				}
+				if (methodBase.IsGenericMethodDefinition)
+					continue;
+				RuntimeHelpers.PrepareMethod(methodBase.MethodHandle);
+			}
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MethodsForm());
